5df4fd69e6cf09d66e585b36d203e12b6f2b2f96
Albert de Nafta <treblaatfan1406@gmail.com>
2024-07-29 16:43:11 +0700

    done

--- src/src/test/java/com/hcmus/mentor/backend/steps/CreateMeetingStepsExcel.java
+++ src/src/test/java/com/hcmus/mentor/backend/steps/CreateMeetingStepsExcel.java
@@ -17,15 +17,20 @@ import java.util.Map;
 
 
 public class CreateMeetingStepsExcel {
-    private final WebDriverWait wait = CommonHooks.wait;
     private final WebDriver driver = CommonHooks.driver;
+    private final WebDriverWait wait = CommonHooks.wait;
     handler handler = new handler();
-    @Khi("người dùng chọn Lịch hẹn, người dùng bấm dấu cộng để thêm cuộc hẹn {string}")
-    public void openFormMeeting(String excelFilePath) throws InterruptedException, IOException {
-        List<Map<String, String>> dataList = handler.readExcelData(excelFilePath);
+    private List<Map<String, String>> dataList;
 
+    @Khi("người dùng chọn Lịch hẹn, người dùng bấm dấu cộng để thêm cuộc hẹn {string}")
+    public void clickAtMeeting(String excelFilePath) throws InterruptedException, IOException {
+        dataList = handler.readExcelData(excelFilePath);
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[3]/div[1]/div[2]/div[7]/div[1]/div[1]/div[1]/div[1]"))).click();
+    }
+
+    @Và("người dùng bấm chọn Tạo lịch hẹn")
+    public void openFormMeeting() throws InterruptedException {
         for (Map<String, String> data : dataList) {
-            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[3]/div[1]/div[2]/div[7]/div[1]/div[1]/div[1]/div[1]"))).click();
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[3]/div[1]/div[1]/div[1]/button[2]"))).click();
 
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html[1]/body[1]/div[2]/div[3]/form[1]/div[1]/div[1]/div[1]/input[1]"))).sendKeys(data.get("Tiêu đề"));
@@ -33,32 +38,28 @@ public class CreateMeetingStepsExcel {
 
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html[1]/body[1]/div[2]/div[3]/form[1]/div[1]/div[3]/div[1]/div[1]/div[1]"))).click();
             handler.selectTime(data.get("Thời gian bắt đầu"));
-            Thread.sleep(1000);
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[normalize-space()='OK']"))).click();
 
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html[1]/body[1]/div[2]/div[3]/form[1]/div[1]/div[3]/div[2]/div[1]/div[1]"))).click();
-            System.out.println(data.get("Thời gian kết thúc"));
             handler.selectTime(data.get("Thời gian kết thúc"));
-            Thread.sleep(1000);
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[normalize-space()='OK']"))).click();
 
-            // Select date
             handler.selectDate(data.get("Ngày"));
-            Thread.sleep(1000);
-            // Enter location
+
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html[1]/body[1]/div[2]/div[3]/form[1]/div[1]/div[4]/div[1]/input[1]"))).sendKeys(data.get("Địa điểm"));
+
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'Tạo lịch hẹn')]"))).click();
             Thread.sleep(1000);
+            popupNotification();
         }
     }
-    @Và("người dùng bấm chọn Tạo lịch hẹn")
-    public void clickButtonToCreate() throws InterruptedException {
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'Tạo lịch hẹn')]"))).click();
-        Thread.sleep(1000);
-    }
+
     @Thì("màn hình sẽ xuất hiện thông báo đã tạo lịch hẹn thành công")
-    public void popupNotifications()  {
+    public void popupNotification() {
         String popupXpath = "//div[@role='status']";
         String popupMessage = driver.findElement(By.xpath(popupXpath)).getText();
         Assert.assertEquals(popupMessage, "Tạo lịch hẹn thành công");
     }
+
+
 }
